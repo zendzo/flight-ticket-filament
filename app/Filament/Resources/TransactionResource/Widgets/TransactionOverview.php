91b67b9b6f3cc90affcd759eefa1c808797ae8fd
M.Z Mustofa <zendzzo@gmail.com>
2025-03-27 00:21:08 +0700

    move widget to dedicated resource widget folder

---
+++ app/Filament/Resources/TransactionResource/Widgets/TransactionOverview.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\TransactionResource\Widgets;
+
+use App\Models\Transaction;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class TransactionOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+          Stat::make('Total Transactions', Transaction::query()->count()),
+          Stat::make('Total Amount (Pending)', 'Rp. ' . number_format(Transaction::query()->where('status', 'pending')->sum('amount'))),
+          Stat::make('Total Amount (Success)', 'Rp. ' . number_format(Transaction::query()->where('status', 'success')->sum('amount'))),
+        ];
+    }
+}
